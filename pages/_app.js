a66296a83d44b1d994e263530ba3f8c5c4c6fef8
Rhett Miller <rmiller@sharpencx.com>
2020-12-11 13:42:43 -0500

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import "tailwindcss/tailwind.css";
+import '../styles/global.scss';
+// import '../styles/global.css';
+
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
